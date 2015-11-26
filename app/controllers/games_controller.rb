c00df8642ebb558c60ee8fdcd2fc1bfb299bfdf7
luissifu@work <luis.e.sifuentes@gmail.com>
2015-11-26 14:46:53 -0600

    changed rss structure

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,13 +2,11 @@ class GamesController < ApplicationController
 
   def index
     @games = Game.order(:release_date)
-  end
-
-  def feed
-    @games = Game.order(:release_date)
     respond_to do |format|
+      format.html
       format.rss { render :layout => false }
     end
   end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   # devise_for :users
 
   resources :games, only: [:index]
-  get 'feed' => 'games#feed'
 end

7c1b8b6410b5a648955dbc8e6f20f364e2d91dc9
luissifu@work <luis.e.sifuentes@gmail.com>
2015-11-26 16:06:18 -0600

    filters

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,11 +2,13 @@ class GamesController < ApplicationController
 
   def index
     @games = Game.order(:release_date)
+    @games = @games.where(console: params[:consoles]) if params[:consoles]
+    @games = @games.where(region: params[:regions]) if params[:regions]
+
     respond_to do |format|
       format.html
       format.rss { render :layout => false }
     end
   end
-
 
 end
